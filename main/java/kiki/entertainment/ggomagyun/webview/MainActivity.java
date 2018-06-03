b87950d515a8cd7b80d77b44bfeec5fc7380d92f
bestkiki <bestkiki3@gmail.com>
2018-06-03 20:42:35 +0900

    Add files via upload

---
+++ main/java/kiki/entertainment/ggomagyun/webview/MainActivity.java
@@ -0,0 +1,19 @@
+package kiki.entertainment.ggomagyun.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webb = (WebView)findViewById(R.id.web1);
+        webb.setWebViewClient(new WebViewClient());
+        webb.loadUrl("http://businessgyun.dothome.co.kr");
+    }
+}
